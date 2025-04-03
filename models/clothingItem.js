d8cd2fd56f4f1d11ebc50b2a2703ea9285cf1a02
Majestyk Vaughns <Majestyk.vaughns@gmail.com>
2025-04-02 23:46:39 -0500

    create express server, create userSchema and clothingSchema

---
+++ models/clothingItem.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const clothingSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  weather: {
+    type: String,
+    required: true,
+    enum: ["hot", "warm", "cold"],
+  },
+  imageUrl: {
+    type: String,
+    required: [true, "Please enter a valid URL"],
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: "You must enter a valid URL",
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("clothing", clothingSchema);
